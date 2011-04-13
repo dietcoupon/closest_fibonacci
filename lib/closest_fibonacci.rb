c812672eaeebfe264574a8ac1248306b3f933be3
Tracy <tracy.cogsdill@gmail.com>
2011-04-13 01:01:48 -0700

    specs are important

--- lib/closest_fibonacci.rb
+++ lib/closest_fibonacci.rb
@@ -1,16 +1,12 @@
 module ClosestFibonacci
   def closest_fibonacci
     i1, i2 = 1, 1
-     while i2 <= self
-         i1, i2 = i2, i1+i2
-         if i1 <= self && self <= i2
-           if self-i1 < i2-self
-             return i1
-           else
-             return i2
-         end
-     end
-   end
+    while i2 <= self
+      i1, i2 = i2, i1+i2
+      if i1 <= self && self <= i2
+        return i1
+      end
+    end
   end
 end
 Fixnum.send(:include, ClosestFibonacci)
--- test/test_closest_fibonacci.rb
+++ test/test_closest_fibonacci.rb
@@ -1,9 +1,11 @@
 class TestFib < MiniTest::Unit::TestCase
   def test_closest
     assert_equal 1, 1.closest_fibonacci
-    assert_equal 13, 12.closest_fibonacci
+    assert_equal 8, 12.closest_fibonacci
     assert_equal 13, 14.closest_fibonacci
     assert_equal 34, 44.closest_fibonacci
-    assert_equal 55, 45.closest_fibonacci
+    assert_equal 34, 45.closest_fibonacci
+    assert_equal 34, 55.closest_fibonacci
+    assert_equal 55, 56.closest_fibonacci
   end
 end
